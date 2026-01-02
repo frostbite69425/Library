c280a26df5f41289e1d690b946dadbfe3492d6ba
frostbite69425 <medhihirak3@gmail.com>
2026-01-02 20:55:23 +0530

    add: reading status toggle button

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 
 const container = document.querySelector("div.container");
 const cardDisplay = document.querySelector("div.card-display");
-const newBook = document.querySelector(".new-book-btn > button");
+const newBookBtn = document.querySelector(".new-book-btn > button");
+
+const modal = document.querySelector("dialog.modal");
+const modalConfirmBtn = document.querySelector("button#modalConfirmBtn");
+const modalTitle = document.querySelector("#book-title");
+const modalAuthor = document.querySelector("#book-author");
+const modalPages = document.querySelector("#book-pages");
+const modalReading = document.querySelector("#book-reading-status");
+const modalNotes = document.querySelector("#book-notes");
 
 // MAIN LOGIC
 
@@ -18,16 +26,17 @@ const myLibrary = [];
 
 // BOOK OBJECT CONSTRUCTOR
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, info) {
   if (!new.target) {
     throw Error("Use the 'new' operator to call the constructor.");
   }
+  this.info = info;
   this.bookUid = Book.prototype.uidGen();
   this.title = title;
   this.author = author;
   this.pages = pages;
   if (read == "yes" || read == "read") {
-    this.read = " read";
+    this.read = "read";
   } else {
     this.read = " not read yet";
   }
@@ -41,16 +50,12 @@ Book.prototype = {
 
 // FUNCTION FOR CREATING BOOKS
 
-function addBookToLibrary(title, author, pages, readingStatus) {
-  let book = new Book(title, author, pages, readingStatus);
+function addBookToLibrary(title, author, pages, readingStatus, info) {
+  let book = new Book(title, author, pages, readingStatus, info);
   myLibrary.push(book);
   // log(myLibrary);
   displayBooks();
 }
-
-addBookToLibrary("Hoola", "me", 1, "yes");
-addBookToLibrary("Hoola2", "not me", 1, "yes");
-addBookToLibrary("Hoola3 the sequel", "me", 100, "no");
 
 // FUNCTION FOR DISPLAYING THE BOOKS ON THE PAGE
 
@@ -65,25 +70,35 @@ function displayBooks() {
     let displayAuthor = document.createElement("div");
     let displayPages = document.createElement("div");
     let displayReadingStatus = document.createElement("div");
+    let displayInfo = document.createElement("div");
     let deleteBtn = document.createElement("button");
+    let toggleReadBtn = document.createElement("button");
 
     cardDisplay.appendChild(completeBook);
     completeBook.appendChild(displayTitle);
     completeBook.appendChild(displayAuthor);
     completeBook.appendChild(displayPages);
     completeBook.appendChild(displayReadingStatus);
+    completeBook.appendChild(displayInfo);
     completeBook.appendChild(deleteBtn);
+    completeBook.appendChild(toggleReadBtn);
 
     displayTitle.textContent = books.title;
     displayAuthor.textContent = books.author;
     displayPages.textContent = books.pages;
     displayReadingStatus.textContent = books.read;
+    displayInfo.textContent = books.info;
     deleteBtn.textContent = "Delete this book from the library";
     completeBook.dataset.UUID = books.bookUid;
     deleteBtn.className = "delete-btn";
+    toggleReadBtn.textContent = "✅";
+    toggleReadBtn.className = "toggle-read-btn";
   }
 
   const deleteBtnList = document.querySelectorAll(".delete-btn");
+  const toggleBtnList = document.querySelectorAll(".toggle-read-btn");
+
+  // FUNCTION FOR DELETING BOOKS USING THE BUTTON
 
   deleteBtnList.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -100,31 +115,65 @@ function displayBooks() {
       }
     });
   });
-}
 
-// const deleteBtnList = document.querySelectorAll(".delete-btn");
+  // FUNCTION FOR TOGGLING READ STATUS
+
+  toggleBtnList.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      i = 0;
+      const uid = e.target.parentNode.dataset.UUID;
+      for (books of myLibrary) {
+        if (books.bookUid == uid) {
+          books.read = books.read === "read" ? "not read yet" : "read";
+          displayBooks();
+        } else {
+          i++;
+        }
+      }
+    });
+  });
+}
 
 // FUNCTION FOR ADDING NEW BOOKS USING THE BUTTON
 
-newBook.addEventListener("click", (e) => {
+newBookBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  addBookToLibrary("Hoola2", "not me", 1, "yes");
+  // addBookToLibrary("Hoola2", "not me", 1, "yes", "i");
+  modal.showModal();
 });
 
-// FUNCTION FOR DELETING BOOKS USING THE BUTTON
+// MODAL LOGIC
 
-// deleteBtnList.forEach((btn) => {
-//   btn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     i = 0;
-//     const uid = e.target.parentNode.dataset.UUID;
-//     for (books of myLibrary) {
-//       if (books.bookUid == uid) {
-//         myLibrary.splice(i, 1);
-//         displayBooks();
-//       } else {
-//         i++;
-//       }
-//     }
-//   });
-// });
+modalConfirmBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  validityCheck = [
+    modalTitle.value,
+    modalAuthor.value,
+    modalPages.value,
+    modalReading.value,
+    modalNotes.value,
+  ];
+
+  for (entry of validityCheck) {
+    if (entry == "default" || entry == "") {
+      modal.close();
+      return;
+    } else {
+      addBookToLibrary(
+        modalTitle.value,
+        modalAuthor.value,
+        modalPages.value,
+        modalReading.value,
+        modalNotes.value
+      );
+
+      modal.close();
+      return;
+    }
+  }
+});
+
+addBookToLibrary("Hoola", "me", 1, "yes", "i");
+addBookToLibrary("Hoola2", "not me", 1, "yes", "i");
+addBookToLibrary("Hoola3 the sequel", "me", 100, "no", "i");
