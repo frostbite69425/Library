1d37fb4209d191d93c4c0a8ec8c92f47ad77905c
frostbite69425 <medhihirak3@gmail.com>
2026-01-05 15:48:05 +0530

    add: modal styles + button interactivity

--- script.js
+++ script.js
@@ -94,16 +94,19 @@ function displayBooks() {
     let displayReadingStatus = document.createElement("div");
     let displayInfo = document.createElement("div");
     let deleteBtn = document.createElement("button");
-    let toggleReadBtn = document.createElement("button");
+    let toggleReadBtn = document.createElement("input");
+    // BUG ?
     let readStsContainer = document.createElement("div");
+
+    toggleReadBtn.setAttribute("type", "checkbox");
 
     cardDisplay.appendChild(completeBook);
     completeBook.appendChild(displayTitle);
     completeBook.appendChild(displayAuthor);
     completeBook.appendChild(displayPages);
     completeBook.appendChild(readStsContainer);
-    readStsContainer.appendChild(displayReadingStatus);
     readStsContainer.appendChild(toggleReadBtn);
+    readStsContainer.appendChild(displayReadingStatus);
     completeBook.appendChild(displayInfo);
     completeBook.appendChild(deleteBtn);
 
@@ -112,10 +115,20 @@ function displayBooks() {
     displayPages.textContent = `Number of pages: ${books.pages}`;
     displayReadingStatus.textContent = books.read;
     displayInfo.textContent = `Note: ${books.info}`;
-    deleteBtn.textContent = "Delete this book from the library";
+
+    if (document.body.classList.contains("light")) {
+      deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffffff"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg> Delete`;
+    } else {
+      deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg> Delete`;
+    }
+
     completeBook.dataset.UUID = books.bookUid;
     deleteBtn.className = "delete-btn";
-    toggleReadBtn.textContent = "✅";
+    if (books.read == "read") {
+      toggleReadBtn.checked = true;
+    } else {
+      toggleReadBtn.checked = false;
+    }
     toggleReadBtn.className = "toggle-read-btn";
   }
 
@@ -136,6 +149,7 @@ function toggleFunction(toggleBtnList) {
       books.readBook(uid);
       // HERE books POINTS TO THE LAST OBJECT IN THE ARRAY. I DON'T KNOW HOW TO FIX THAT YET BUT THE LOGIC WORKS AS INTENDED.
     });
+    btn.focus({ preventScroll: true });
   });
 }
 
@@ -193,14 +207,6 @@ modalCancelBtn.addEventListener("click", () => {
   form.reset();
 });
 
-// function setSessionStorage(title, author, pages, readingStatus, notes) {
-//   sessionStorage.bookTitle = title;
-//   sessionStorage.bookAuthor = author;
-//   sessionStorage.bookPages = pages;
-//   sessionStorage.bookReadingStatus = readingStatus;
-//   sessionStorage.bookNotes = notes;
-// }
-
 // THEME SWITCHING FUNCTION
 
 themeBtn.addEventListener("click", (e) => {
@@ -210,6 +216,7 @@ themeBtn.addEventListener("click", (e) => {
 
   if (themeBtn.classList.contains("light")) {
     themeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>`;
+    displayBooks();
   } else {
     themeBtn.innerHTML = `<svg
             xmlns="http://www.w3.org/2000/svg"
@@ -221,6 +228,7 @@ themeBtn.addEventListener("click", (e) => {
             <path
               d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"
             />`;
+    displayBooks();
   }
 });
 
